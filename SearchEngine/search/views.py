12872b09e2e19c9b1e8005a49d41e5277611b823
王拓为 <2018011917@secoder.net>
2022-05-24 01:46:39 +0800

    Add: Finish Frontend

--- SearchEngine/search/views.py
+++ SearchEngine/search/views.py
@@ -4,12 +4,20 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    if request.method == 'GET':
+        print(request.GET.get('query'))
     return render(request, 'search/index.html')
 
 
 def search(request):
+    if request.method == 'GET':
+        print(request.GET.get('query'))
     return render(request, 'search/result.html')
 
 
 def detail(request):
     return render(request, 'search/detail.html')
+
+
+def test(request):
+    return render(request, 'search/test.html')
