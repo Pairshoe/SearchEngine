57d131e9876758ec4e44884c8ec8df155ef13e78
王拓为 <2018011917@secoder.net>
2022-06-06 20:30:08 +0800

    Add: sort

--- SearchEngine/search/views.py
+++ SearchEngine/search/views.py
@@ -17,8 +17,13 @@ def search(request):
     content = {}
     template = loader.get_template('search/result.html')
     if request.method == 'GET':
+        if request.GET.get('sort_key') is not None:
+            sort_key = request.GET.get('sort_key')
+        else:
+            sort_key = ''
+        content['sort_key'] = sort_key
         if request.GET.get('query') is not None:
-            searchEngineCore.make_query(request.GET.get('query'))
+            searchEngineCore.make_query(request.GET.get('query'), sort_key=sort_key)
             content['query'] = request.GET.get('query')
         elif request.GET.get('keyword') is not None:
             condition_list = {
@@ -31,20 +36,23 @@ def search(request):
                 'court': request.GET.get('court'),
                 'document_type': request.GET.get('document_type'),
             }
-            searchEngineCore.make_query(request.GET.get('keyword'), conditions=condition_list, accurate_mode=True)
+            searchEngineCore.make_query(request.GET.get('keyword'), conditions=condition_list, accurate_mode=True,
+                                        sort_key=sort_key)
             content['keyword'] = request.GET.get('keyword')
         else:
-            searchEngineCore.make_query(request.GET.get('case_content'))
+            searchEngineCore.make_query(request.GET.get('case_content'), sort_key=sort_key)
             content['case_content'] = request.GET.get('case_content')
         if request.GET.get('current_page') is not None:
-            results = searchEngineCore.search_case(int(request.GET.get('current_page')))
+            current_page = int(request.GET.get('current_page'))
+            results = searchEngineCore.search_case(current_page)
         else:
             results = searchEngineCore.search_case(1)
+            current_page = 1
         content['results'] = results[0]
         content['page_num'] = results[1]
-        content['current_page'] = int(request.GET.get('current_page'))
-        content['prev_page'] = content['current_page'] - 1
-        content['next_page'] = content['current_page'] + 1
+        content['current_page'] = current_page
+        content['prev_page'] = current_page - 1
+        content['next_page'] = current_page + 1
         content['last_page'] = content['page_num'][-1]
         for result in results[0]:
             result['highlight'] = ''.join(result['highlight'])
@@ -55,7 +63,6 @@ def detail(request):
     template = loader.get_template('search/detail.html')
     if request.method == 'GET':
         results = searchEngineCore.get_case(request.GET.get('id'))
-        print(results)
     return HttpResponse(template.render(results, request))
 
 
